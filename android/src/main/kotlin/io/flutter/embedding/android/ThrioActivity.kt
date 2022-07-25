56f30d3f76930cf9450b4c7d76ce1f1b52d86ac6
shangguanyanyan <1024503335@qq.com>
2022-07-25 17:52:28 +0800

    feat:适配全面屏

--- android/src/main/kotlin/io/flutter/embedding/android/ThrioActivity.kt
+++ android/src/main/kotlin/io/flutter/embedding/android/ThrioActivity.kt
@@ -25,6 +25,8 @@ package io.flutter.embedding.android
 
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import com.hellobike.flutter.thrio.BooleanCallback
 import com.hellobike.flutter.thrio.NullableBooleanCallback
 import com.hellobike.flutter.thrio.extension.getEntrypoint
@@ -39,6 +41,17 @@ open class ThrioActivity : FlutterActivity() {
         intent.putExtra(NAVIGATION_ROUTE_ENTRYPOINT_KEY, cachedEngineId)
 
         super.onCreate(savedInstanceState)
+
+        // 延伸显示区域到刘海
+        // 延伸显示区域到刘海
+        val lp = window.attributes
+        lp.layoutInDisplayCutoutMode =
+            WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+        // 设置页面全屏显示
+        window.attributes = lp
+        val decorView = window.decorView
+        decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
     }
 
     private var _initialEntrypoint: String? = null
